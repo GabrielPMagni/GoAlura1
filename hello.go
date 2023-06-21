63ea24c26b6cbd873473d75400f505077a7e561b
Gabriel Peres Magni <me@gpmagni.dev.br>
2023-06-21 17:48:17 -0300

    v1.0

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,6 +24,7 @@ func main() {
 			startMonitoring()
 		case 2:
 			fmt.Println("Exbindo logs")
+			showLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -31,7 +37,7 @@ func main() {
 
 func startMonitoring() {
 	fmt.Println("Monitorando")
-	websites := []string{"https://www.alura.com.br", "https://www.caelum.com.br"}
+	websites := getLinesFromFile()
 	for {
 		for _, website := range websites {
 			testWebsite(website)
@@ -45,8 +51,10 @@ func testWebsite(website string) {
 	response, err := http.Get(website)
 	if response.StatusCode == 200 && err == nil {
 		fmt.Println("Site:", website, "Sucesso")
+		registerLog(website, true)
 	} else {
 		fmt.Println("Site:", website, "Falha")
+		registerLog(website, false)
 	}
 }
 
@@ -69,4 +77,49 @@ func getMenuOption() int {
 	var command int
 	fmt.Scan(&command)
 	return command
+}
+
+func getLinesFromFile() []string {
+	var sites []string
+	file, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Falha ao abrir arquivo de sites")
+		os.Exit(-1)
+	}
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		sites = append(sites, line)
+		if err == io.EOF {
+			break
+		}
+	}
+	file.Close()
+	return sites
+}
+
+func registerLog(website string, status bool) {
+	logFile, err := os.OpenFile("events.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Falha ao abrir arquivo de logs")
+	}
+
+	if _, err := logFile.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + website + " - ONLINE: " + strconv.FormatBool(status) + "\n"); err != nil {
+		logFile.Close()
+	}
+
+	if err := logFile.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func showLogs() {
+	file, err := os.ReadFile("events.log")
+	if err != nil {
+		fmt.Println("Falha ao abrir arquivo de logs: ", err)
+		os.Exit(-1)
+	}
+
+	fmt.Println(string(file))
 }
